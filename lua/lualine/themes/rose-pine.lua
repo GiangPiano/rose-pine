e087b2ffb6d51bf8cf7e7733dbed3b311a47e3dd
GiangPiano <157930152+GiangPiano@users.noreply.github.com>
2025-06-05 11:31:13 +1000

    Remove bold in lualine

--- lua/lualine/themes/rose-pine.lua
+++ lua/lualine/themes/rose-pine.lua
@@ -8,32 +8,32 @@ end
 
 return {
 	normal = {
-		a = { bg = p.rose, fg = p.base, gui = "bold" },
+		a = { bg = p.rose, fg = p.base },
 		b = { bg = p.overlay, fg = p.rose },
 		c = { bg = bg_base, fg = p.text },
 	},
 	insert = {
-		a = { bg = p.foam, fg = p.base, gui = "bold" },
+		a = { bg = p.foam, fg = p.base },
 		b = { bg = p.overlay, fg = p.foam },
 		c = { bg = bg_base, fg = p.text },
 	},
 	visual = {
-		a = { bg = p.iris, fg = p.base, gui = "bold" },
+		a = { bg = p.iris, fg = p.base },
 		b = { bg = p.overlay, fg = p.iris },
 		c = { bg = bg_base, fg = p.text },
 	},
 	replace = {
-		a = { bg = p.pine, fg = p.base, gui = "bold" },
+		a = { bg = p.pine, fg = p.base },
 		b = { bg = p.overlay, fg = p.pine },
 		c = { bg = bg_base, fg = p.text },
 	},
 	command = {
-		a = { bg = p.love, fg = p.base, gui = "bold" },
+		a = { bg = p.love, fg = p.base },
 		b = { bg = p.overlay, fg = p.love },
 		c = { bg = bg_base, fg = p.text },
 	},
 	inactive = {
-		a = { bg = bg_base, fg = p.muted, gui = "bold" },
+		a = { bg = bg_base, fg = p.muted },
 		b = { bg = bg_base, fg = p.muted },
 		c = { bg = bg_base, fg = p.muted },
 	},
